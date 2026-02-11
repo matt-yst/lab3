ff7465fef870da200c1376ea304b53bf4da8faba
matt-yst <matthew.ys.tan@gmail.com>
2026-02-10 20:20:05 -0500

    skeleton of main initialized

--- main.py
+++ main.py
@@ -2,3 +2,26 @@ import collect_color_sensor_data
 import collect_us_sensor_data
 import touch_sensor
 
+def read():
+    # Reads all sensor input data, output string
+    return output
+
+if __name__ == "__main__":
+    while True:
+        out = read()
+        match out:
+            case "ES":
+                #emergency stop
+                
+            case "DT":
+                #drum toggle
+            case "N1":
+                #note 1
+            case "N2":
+                #note 2
+            case "N3":
+                #note 3 
+            case "N4":
+                #note 4
+            case _:
+                pass
