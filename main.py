ea3dab275835d0697a42243a0c1527b4b7651bf4
matt-yst <matthew.ys.tan@gmail.com>
2026-02-13 02:08:24 -0500

    read function added

--- main.py
+++ main.py
@@ -2,8 +2,33 @@ import collect_color_sensor_data
 import collect_us_sensor_data
 import touch_sensor
 
+US_LOWER_BOUND = 3
+US_UPPER_BOUND = 3
+
+def us_in_range(data):
+    if data < US_UPPER_BOUND and data > US_LOWER_BOUND:
+        return True
+    else:
+        return False
+    
 def read():
     # Reads all sensor input data, output string
+    us_data = US_SENSOR.get_value()
+
+    if us_in_range(us_data) and not touch1.is_pressed() and touch2.is_pressed():
+        output = "ES"
+    elif not us_in_range(us_data) and not touch1.is_pressed() and touch2.is_pressed():
+        output = "DT"
+    elif us_in_range(us_data) and not touch1.is_pressed() and touch2.is_pressed():
+        output = "N1"
+    elif not us_in_range(us_data) and touch1.is_pressed() and not touch2.is pressed():
+        output = "N2"
+    elif us_in_range(us_data) and touch1.is_pressed() and not touch2.is_pressed():
+        output = "N3"  
+    elif not us_in_range(us_data) and touch1.is_pressed() and touch2.is_pressed():
+        output = "N4"
+    else:
+        output = "None"
     return output
 
 if __name__ == "__main__":
