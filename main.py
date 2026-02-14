14093755b8a6b024c6cc13f07358f5da09da12aa
Sonia Ly <sonia.ly@mail.mcgill.ca>
2026-02-13 23:12:12 -0500

    commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils import sound
 from utils.brick import TouchSensor, Motor, EV3UltrasonicSensor, wait_ready_sensors
 import time
+import threading
 
 # -------------------- SENSORS AND MOTORS --------------------
 
